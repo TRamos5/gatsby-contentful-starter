d178b92fdedff5563e3ff7dc2898f77f52c856b4
Travis Ramos <travis.ramos5@gmail.com>
2019-07-23 22:45:27 -0500

    testing form

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,6 @@ import { StaticQuery, graphql } from "gatsby"
 // import SEO from "../components/seo"
 import Layout from "../components/homeLayout"
 import Particle from "../components/particle"
-import ContactForm from '../components/contactForm'
 
 const style = ({
   wrapper: {
@@ -69,7 +68,6 @@ const HomePage = () => (
         <div style={style.main}>
           <h1 style={style.title}>{title}</h1>
           <h3 style={style.content}>{content}</h3>
-          <ContactForm />
         </div>
       </div>
       </Layout>
