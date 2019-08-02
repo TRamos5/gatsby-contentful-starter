d7878bf23267de8bf3fb9a7cb060aadf3e743422
Travis Ramos <travis.ramos5@gmail.com>
2019-08-01 20:24:25 -0500

    hooked up blog

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,39 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require(`path`)
+const slash = require(`slash`);
 
-// You can delete this file if you're not using it
+exports.createPages = ({ graphql, actions }) => {
+    const { createPage } = actions
+    const blogPostTemplate = path.resolve(`src/templates/blogpost.js`)
+
+    return graphql(`
+    {
+        allContentfulBlogPost {
+          edges {
+            node {
+              id
+              slug
+            }
+          }
+        }
+      }
+    `).then(result => {
+        if (result.errors) {
+            console.log("Error retrieving contentful data", result.errors);
+            throw result.errors
+        }
+
+        result.data.allContentfulBlogPost.edges.forEach(edge => {
+            createPage({
+                path: `/blogpost/${edge.node.slug}/`,
+                component: slash(blogPostTemplate),
+                context: {
+                    slug: edge.node.slug,
+                    id: edge.node.id
+                }
+            })
+        })
+    })
+    .catch(error => {
+        console.log("Error retrieving contentful data", error)
+    })
+}
